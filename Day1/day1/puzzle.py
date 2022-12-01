cd8cdd1579c6d2e004e7b82c428a4e8324f4c498
StormicK <andre.bruns33@gmail.com>
2022-12-02 00:04:38 +0100

    Test

--- Day1/day1/puzzle.py
+++ Day1/day1/puzzle.py
@@ -1,22 +1,32 @@
 def main():
-    sorted_elves = sorted(get_elves())
-    print(f'Answer 1: {sorted_elves[-1]}')
-    print(f'Answer 2: {sum(sorted_elves[-3:])}')
+    sorted_calories = sorted([elve.sum() for elve in get_elves()])
+    print(f'Answer 1: {sorted_calories[-1]}')
+    print(f'Answer 2: {sum(sorted_calories[-3:])}')
 
 def get_elves():
     with open('input.txt') as input:
         lines = input.readlines()
     result = []
-    elve = []
+    elve = Elve()
     for line in lines:
         if not line.strip():
             result.append(elve)
-            elve = []
+            elve = Elve()
         else:
             elve.append(int(line))
-    if elve:
+    if elve.sum():
         result.append(elve)
-    return [sum(value) for value in result]
+    return result
+
+class Elve:
+    def __init__(self):
+        self.food = []
+    
+    def sum(self):
+        return sum(self.food)
+    
+    def append(self, food):
+        self.food.append(food)
 
 
 if __name__ == '__main__':
